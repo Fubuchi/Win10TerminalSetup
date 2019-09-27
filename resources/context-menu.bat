fe3e9a9ce150c56929ff3b8ad75a1915be8fbed8
Fubuchi <haiuit9795@gmail.com>
2019-09-27 19:14:53 +0700

    Update context menu guide

---
+++ resources/context-menu.bat
@@ -0,0 +1,8 @@
+@echo off
+
+reg.exe add "HKEY_CLASSES_ROOT\Directory\Background\shell\wt" /f /ve /d "Windows Terminal here"
+reg.exe add "HKEY_CLASSES_ROOT\Directory\Background\shell\wt" /f /v "Icon" /t REG_EXPAND_SZ /d "\"%%USERPROFILE%%\Pictures\cmder_purple.ico\""
+
+reg.exe add "HKEY_CLASSES_ROOT\Directory\Background\shell\wt\command" /f /ve /t REG_EXPAND_SZ /d "\"%%LOCALAPPDATA%%\Microsoft\WindowsApps\wt.exe\""
+
+pause
